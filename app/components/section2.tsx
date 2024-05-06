576a3261e1bf73ecbb4a91c8efc354f3ead79d9a
Souhani <souhanikarim1999@gmail.com>
2024-05-06 21:38:25 +0200

    gradient color change fixed

--- app/components/section2.tsx
+++ app/components/section2.tsx
@@ -29,11 +29,13 @@ const Section2: FC = () => {
               </div>
             ))}
           </div>
-          <button className="w-max font-Satoshi_B rounded-md text-[18px] 1000px:px-[225px] px-[125px]  py-5  bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] mt-2 group relative overflow-hidden hover:to-[#3D8BFF] transition-all duration-500 ease-in-out">
-  <span className="w-full absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 transition-all duration-500 ease-in-out group-hover:translate-y-[-65px] ">
+          <button className=" w-max font-Satoshi_B rounded-md text-[18px] 1000px:px-[225px] px-[125px]  py-5  mt-2 group relative overflow-hidden transition-all duration-500 ease-in-out">
+          <div className="absolute w-full h-full  bg-gradient-to-r from-[#3D8BFF] to-[#AB23FF] opacity-100 group-hover:opacity-0 left-0 top-0 transition-all duration-[1s] ease-in-out"></div>
+            <div className="absolute w-full h-full bg-blue-500  left-0 top-0 z-[-9] transition-all duration-[1s] ease-in-out"></div>
+  <span className="w-full absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 transition-all duration-[1s] ease-in-out group-hover:translate-y-[-65px] ">
     Buy Creon Pass
   </span>
-  <span className="w-full absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[100%] transition-all duration-500 ease-in-out group-hover:translate-y-[-7px] ">
+  <span className="w-full absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[100%] transition-all duration-[1s] ease-in-out group-hover:translate-y-[-7px] ">
     Buy Creon Pass
   </span>
 </button>
